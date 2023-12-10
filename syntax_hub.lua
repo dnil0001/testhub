3428a7092505e5003572b2c8b3ef0cb16f67413e
dnil0001 <136042543+dnil0001@users.noreply.github.com>
2023-12-09 18:32:40 -0800

    Create syntax_hub.lua

---
+++ syntax_hub.lua
@@ -0,0 +1,23 @@
+--check game func
+if game.PlaceId == 155615604 then
+    local CurrentVerison = "string text"
+    --create lib
+  local Mercury = loadstring(game:HttpGet("https://raw.githubusercontent.com/deeeity/mercury-lib/master/src.lua"))()
+   --creating main frame
+  local GUI = Mercury:Create{
+    Name = "Mercury",
+    Size = UDim2.fromOffset(600, 400),
+    Theme = Mercury.Themes.Dark,
+    Link = "https://github.com/deeeity/mercury-lib"
+      -- Local tab
+      
+    local hood = GUI:Tab{
+        Name = "New Tab",
+        Icon = "rbxassetid://8569322835"
+    }
+
+
+
+}
+end --check read the func of first game place ID
+
