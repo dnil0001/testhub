5b0839c08ca284fdf540aa69dbaf78de14d93bf0
dnil0001 <136042543+dnil0001@users.noreply.github.com>
2023-12-10 15:41:10 -0800

    Update testing.lua

--- testing.lua
+++ testing.lua
@@ -1,179 +1,54 @@
-local Library = loadstring(game:HttpGet('https://raw.githubusercontent.com/dnil0001/mainUI/main/mainuilib.lua'))()
-local Flags = Library.Flags
-print("im part of the kkk niggers")
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Player = game.Players.LocalPlayer
+local Window = OrionLib:MakeWindow({Name = "Key System", HidePremium = false, SaveConfig = true, IntroEnabled = false})
 
-function myTest()
-    print("Working!!!!!!!!!!!!!")
+OrionLib:MakeNotification({
+	Name = "Title!",
+	Content = "You are logged in as"..Player.Name..".",
+	Image = "rbxassetid://4483345998",
+	Time = 5
+})
+
+G.Key = "Hello"
+G.KeyInput = "string"
+
+function valid()
+    print("Its valid guysss!")
 end
 
-local Window = Library:Window({
-    Text = "Baseplate"
-})
-
-local Tab = Window:Tab({
-    Text = "Aiming"
-})
-
-local Tab2 = Window:Tab({
-    Text = "Visual"
-})
-
-local ChamsSection = Tab2:Section({
-    Text = "Chams"
-})
-
-local Section = Tab:Section({
-    Text = "Aimbot"
-})
-
-local Section2 = Tab:Section({
-    Text = "FOV"
-})
-
-local Section3 = Tab:Section({
-    Text = "Misc",
-    Side = "Right"
-})
-
-ChamsSection:Toggle({
-    Text = "Enabled"
-})
-
-ChamsSection:Toggle({
-    Text = "Color"
-})
-
-ChamsSection:Toggle({
-    Text = "Filled"
-})
-
-ChamsSection:Toggle({
-    Text = "Team Check"
-})
-
-Section:Toggle({
-    Text = "Enabled"
-})
-
-Section:Toggle({
-Text = "Wall Check",
-Callback = function()
-print("button pressed")
-myTest()
+function invalid()
+    print("inst valid guyss")
 end
+
+function Destroy()
+    game:GetService("CoreGui").Orion:Destroy() 
+end
+
+local Tab = Window:MakeTab({
+	Name = "Key",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
 })
 
-Section:Toggle({
-    Text = "Smooth Aimbot",
-    Callback = myTest()
+Tab:AddTextbox({
+	Name = "Enter Key",
+	Default = "",
+	TextDisappear = true,
+	Callback = function(Value)
+		print(Value)
+	end	  
 })
 
-Section2:Toggle({
-    Text = "Enabled",
-    Callback = function()
-        myTest()
-    end
+Tab:AddButton({
+	Name = "Check Key!",
+	Callback = function()
+      		if G.KeyInput == G.Key then
+                Destroy()
+                local Window = OrionLib:MakeWindow({Name = "Works!!", HidePremium = false, SaveConfig = true, IntroEnabled = false})
+                local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+                valid()
+            else
+                invalid()
+            end
+  	end    
 })
-
-Section2:Toggle({
-    Text = "Filled FOV",
-    Callback = function()
-        print("button pressed")
-    end
-})
-
-Section2:Toggle({
-    Text = "FOV Transparency",
-    Tooltip = "Changes your fov transparency."
-})
-
-Section2:Button({
-    Text = "Reset FOV",
-    Tooltip = "This resets your aimbot fov."
-})
-
-Section3:Toggle({
-    Text = "Infinite Ammo"
-})
-
-Section3:Toggle({
-    Text = "No Spread"
-})
-
-Section3:Toggle({
-    Text = "No Bullet Drop",
-    Default = true
-})
-
-Section3:Toggle({
-    Text = "Full Auto"
-})
-
-local a = Section3:Toggle({
-    Text = "No Recoil"
-})
-
-local label = Section3:Label({
-    Text = "This is a label.",
-    Color = Color3.fromRGB(217, 97, 99),
-    Tooltip = "This is a label."
-})
-
-local dropdown = Section:Dropdown({
-    Text = "Dropdown",
-    List = {"Head","Torso","Random"},
-    Flag = "Choosen",
-    Callback = function(v)
-        warn(v)
-    end
-})
-
-Section:RadioButton({
-    Text = "RadioButton",
-    Options = {"Legit","Blatant"},
-    Callback = function(v)
-        warn(v)
-    end
-})
-
-Section:Toggle({
-    Text = "Silent Aimbot"
-})
-
-Section:Input({
-    Placeholder = "Webhook URL",
-    Flag = "URL"
-})
-
-Section:Keybind({
-    Default = Enum.KeyCode.E,
-    Text = "Aimbot Key",
-    Callback = function()
-        warn("Pressed")
-    end
-})
-
-Section:Slider({
-    Text = "Slider Test",
-    Default = 5,
-    Minimum = 0,
-    Maximum = 50,
-    Flag = "SliderFlag",
-    Callback = function(v)
-        warn(v)
-    end
-})
-
-Tab:Select()
-
-wait(5)
-
---[[dropdown:Refresh({
-    List = {"Head", "Feet"}
-})--]]
-
-label:Set({
-    Text = "This is a red label."
-})
-
-a:Set(true)
